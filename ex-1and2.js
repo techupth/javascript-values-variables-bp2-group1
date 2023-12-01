4acae13a439f3177d28d6c60d3a43182ef13fa79
Pawat <as.pawat@gmail.com>
2023-12-01 15:44:38 +0800

    Pup

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,10 @@
 //Exercise #1: Variable Creation
-//Start coding here
+let Name = Karter;
+let Surname = Anderson;
+Let Address = 54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai;
+const restaurantName = "Chef's Table by Chef James";
+const totalBill = 15000;
+const totalBranches = 1500;
 
 //Exercise #2: Variable Reassignment
 //Start coding here
